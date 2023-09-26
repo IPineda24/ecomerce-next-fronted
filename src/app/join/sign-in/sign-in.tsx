d95d6e9144d68f0bf656895d6a5018c098f30350
Irvin Pineda <irvin.pineda024@gmail.com>
2023-09-26 14:28:44 -0600

    :art: Add join layouts

---
+++ src/app/join/sign-in/sign-in.tsx
@@ -0,0 +1,16 @@
+import styles from "./sing-in.module.scss"
+import { JoinLayout } from "@/layouts"
+export default function SignInPage() {
+    return (
+        <>
+
+            <JoinLayout>
+
+                <div>
+                    <h3>Iniciar sesión</h3>
+                </div>
+
+            </JoinLayout>
+        </>
+    )
+}
---
+++ src/app/join/sign-up/sign-up.tsx
@@ -0,0 +1,15 @@
+
+import styles from "./sing-up.module.scss";
+import { JoinLayout } from "@/layouts"
+export default function SignUpPage() {
+    return (
+        <>
+            <JoinLayout>
+                <div>
+                    <h3>Crear cuenta</h3>
+                </div>
+            </JoinLayout>
+        
+        </>
+    )
+}
